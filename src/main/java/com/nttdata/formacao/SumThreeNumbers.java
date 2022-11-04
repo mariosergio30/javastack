32a0bd1835332a3f05e1f734aab1ade4c1e0a1b8
smarqsil <sofia.marquessilva@emeal.nttdata.com>
2022-11-04 15:06:17 +0000

    TK06-add:class and method

---
+++ src/main/java/com/nttdata/formacao/SumThreeNumbers.java
@@ -0,0 +1,12 @@
+package com.nttdata.formacao;
+
+public class SumThreeNumbers {
+    private int a;
+    private int b;
+    private int c;
+
+    public int sumThreeNumbers(){
+        return (this.a + this.b + this.c);
+    }
+
+}
