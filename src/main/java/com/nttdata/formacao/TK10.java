80bd3e546ee29429116340e837e3f0798818e029
USERSAD\jteixeso <jteixeso@everis.nttdata.com>
2022-11-04 15:24:24 +0000

    Add TK10 feature

---
+++ src/main/java/com/nttdata/formacao/TK10.java
@@ -0,0 +1,11 @@
+package com.nttdata.formacao;
+
+public class TK10 {
+
+    public String addString(String a, String b, String c){
+
+        String str = a.concat(b).concat(c);
+
+        return str;
+    }
+}
