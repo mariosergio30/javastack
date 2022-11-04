7e8a3f34b6da453ecd6c68e0b44eda39115e824a
joseFranAguiar <jose.franciscoaguiar.st@everis.nttdata.com>
2022-11-04 15:29:02 +0000

    SumTwoNumbers added

--- src/main/java/com/nttdata/formacao/SumTwoNumbers.java
+++ src/main/java/com/nttdata/formacao/SumTwoNumbers.java
@@ -1,2 +1,16 @@
-package com.nttdata.formacao;public class SumTwoNumbers {
+package com.nttdata.formacao;
+
+import java.util.Scanner;
+
+public class SumTwoNumbers {
+    private int num1;
+    private int num2;
+
+    private int sumTwoNumbers(int num1, int num2)
+    {
+        Scanner scanner = new Scanner(System.in);
+        num1 = scanner.nextInt();
+        num2 = scanner.nextInt();
+        return num1+num2;
+    }
 }
