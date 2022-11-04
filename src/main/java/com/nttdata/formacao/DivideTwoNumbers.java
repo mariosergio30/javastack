ce9d0c4a97cd5bb00176b31e369db51ba36fa7ff
USERSAD\jhenriqa <joao.henrique.anjos@everis.com>
2022-11-04 15:21:13 +0000

    TK04-Divide Two Numbers

---
+++ src/main/java/com/nttdata/formacao/DivideTwoNumbers.java
@@ -0,0 +1,14 @@
+package com.nttdata.formacao;
+
+public class DivideTwoNumbers {
+    public Double a = 10D;
+    public Double b = 14D;
+    public Double result;
+
+    public Double divideTwoNumbers(){
+
+        result = a / b;
+
+        return result;
+    }
+}
